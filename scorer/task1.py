3659d4df73f51febbd95e3ebb0128e3d77083b24
Giovanni <joedsm@gmail.com>
2020-11-08 19:41:00 +0100

    added file logging and removed printing of gold file path

--- scorer/task1.py
+++ scorer/task1.py
@@ -1,6 +1,6 @@
 import pdb
 import json
-import logging
+import logging.handlers
 import argparse
 import os
 from sklearn.metrics import f1_score
@@ -14,7 +14,13 @@ from format_checker.task1 import check_format_task1
 Scoring of SEMEVAL-Task-6--subtask-1 with the metrics f1-macro and f1-micro. 
 """
 
-logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
+logger = logging.getLogger("task1_scorer")
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.setLevel(logging.INFO)
+#logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
 
 def read_classes(file_path):
   CLASSES = []
@@ -39,14 +45,15 @@ def _read_gold_and_pred(pred_fpath, gold_fpath):
   :return: {id:pred_labels} dict; {id:gold_labels} dict
   """
 
-  logging.info("Reading gold predictions from file {}".format(gold_fpath))
+  if args.detailed_results:
+    logger.info("Reading gold predictions from file {}".format(gold_fpath))
   gold_labels = {}
   with open(gold_fpath, encoding='utf-8') as gold_f:
     gold = json.load(gold_f)
     for obj in gold:
       gold_labels[obj['id']] = obj['labels']
 
-  logging.info('Reading predicted ranking order from file {}'.format(pred_fpath))
+  logger.info('Reading predicted ranking order from file {}'.format(pred_fpath))
   pred_labels = {}
   with open(pred_fpath, encoding='utf-8') as pred_f:
     pred = json.load(pred_f)
@@ -54,7 +61,7 @@ def _read_gold_and_pred(pred_fpath, gold_fpath):
       pred_labels[obj['id']] = obj['labels']
 
   if set(gold_labels.keys()) != set(pred_labels.keys()):
-      logging.error('There are either missing or added examples to the prediction file. Make sure you only have the gold examples in the prediction file.')
+      logger.error('There are either missing or added examples to the prediction file. Make sure you only have the gold examples in the prediction file.')
       raise ValueError('There are either missing or added examples to the prediction file. Make sure you only have the gold examples in the prediction file.')
 
   return pred_labels, gold_labels
@@ -90,7 +97,7 @@ def evaluate(pred_fpath, gold_fpath, CLASSES):
 
 def validate_files(pred_files, gold_files, CLASSES):
   if not check_format_task1(pred_file, CLASSES):
-    logging.error('Bad format for pred file {}. Cannot score.'.format(pred_file))
+    logger.error('Bad format for pred file {}. Cannot score.'.format(pred_file))
     return False
   return True
 
@@ -128,14 +135,24 @@ if __name__ == '__main__':
   pred_file = args.pred_file_path
   gold_file = args.gold_file_path
 
+  if not args.detailed_results:
+    output_log_file = pred_file + ".log"
+    logger.info("Logging execution to file " + output_log_file)
+    fileLogger = logging.FileHandler(output_log_file)
+    fileLogger.setLevel(logging.DEBUG)
+    fileLogger.setFormatter(formatter)
+    logger.addHandler(fileLogger)
+  else:
+     logger.addHandler(ch)
+
   if not os.path.exists(args.classes_file_path):
-    logging.errors("File doesnt exists: {}".format(classes_file_path))
+    logger.errors("File doesnt exists: {}".format(classes_file_path))
     raise ValueError("File doesnt exists: {}".format(classes_file_path))
   CLASSES = read_classes(args.classes_file_path)
 
   if validate_files(pred_file, gold_file, CLASSES):
     macro_f1, micro_f1 = evaluate(pred_file, gold_file, CLASSES)
     if args.detailed_results:
-      logging.info("macro-F1={:.5f}\tmicro-F1={:.5f}".format(macro_f1, micro_f1))
+      logger.info("macro-F1={:.5f}\tmicro-F1={:.5f}".format(macro_f1, micro_f1))
     else:
-      logging.info("{}\t{}".format(macro_f1, micro_f1))
+      logger.info("{}\t{}".format(macro_f1, micro_f1))
