f986f5c3685ae66596c25bfd11ed647a491029a7
Giovanni <joedsm@gmail.com>
2020-11-17 14:50:57 +0100

    fixing logging for task 1 and 3

--- scorer/task1_3.py
+++ scorer/task1_3.py
@@ -37,15 +37,12 @@ def _read_gold_and_pred(pred_fpath, gold_fpath):
   :return: {id:pred_labels} dict; {id:gold_labels} dict
   """
 
-  if args.detailed_results:
-    logger.info("Reading gold predictions from file {}".format(gold_fpath))
   gold_labels = {}
   with open(gold_fpath, encoding='utf-8') as gold_f:
     gold = json.load(gold_f)
     for obj in gold:
       gold_labels[obj['id']] = obj['labels']
 
-  logger.info('Reading predicted ranking order from file {}'.format(pred_fpath))
   pred_labels = {}
   with open(pred_fpath, encoding='utf-8') as pred_f:
     pred = json.load(pred_f)
@@ -55,7 +52,7 @@ def _read_gold_and_pred(pred_fpath, gold_fpath):
   if set(gold_labels.keys()) != set(pred_labels.keys()):
       logger.error('There are either missing or added examples to the prediction file. Make sure you only have the gold examples in the prediction file.')
       raise ValueError('There are either missing or added examples to the prediction file. Make sure you only have the gold examples in the prediction file.')
-
+  
   return pred_labels, gold_labels
 
 def evaluate(pred_fpath, gold_fpath, CLASSES):
@@ -109,11 +106,11 @@ if __name__ == '__main__':
     help="Path to the file with predictions"
   )
   parser.add_argument(
-    "--detailed_results",
-    "-d",
+    "--log_to_file",
+    "-l",
     action='store_true',
     default=False,
-    help="Set flag if you want detailed results printed."
+    help="Set flag if you want to log the execution file. The log will be appended to <pred_file>.log"
   )
   parser.add_argument(
     "--classes_file_path", 
@@ -126,26 +123,33 @@ if __name__ == '__main__':
   pred_file = args.pred_file_path
   gold_file = args.gold_file_path
 
-  if not args.detailed_results:
+  if args.log_to_file:
     output_log_file = pred_file + ".log"
     logger.info("Logging execution to file " + output_log_file)
     fileLogger = logging.FileHandler(output_log_file)
     fileLogger.setLevel(logging.DEBUG)
     fileLogger.setFormatter(formatter)
     logger.addHandler(fileLogger)
-  else:
-     logger.addHandler(ch)
+    logger.setLevel(logging.DEBUG) #
 
   if not os.path.exists(args.classes_file_path):
     logger.errors("File doesnt exists: {}".format(classes_file_path))
     raise ValueError("File doesnt exists: {}".format(classes_file_path))
   CLASSES = read_classes(args.classes_file_path)
 
+  if args.log_to_file:
+    logger.info('Reading gold file')
+  else:
+    logger.info("Reading gold predictions from file {}".format(args.gold_file_path))
+  if args.log_to_file:
+    logger.info('Reading predictions file')
+  else:
+    logger.info('Reading predictions file {}'.format(args.pred_file_path))
+
   if validate_files(pred_file, gold_file, CLASSES):
+    logger.info('Prediction file format is correct')
     macro_f1, micro_f1 = evaluate(pred_file, gold_file, CLASSES)
-    if args.detailed_results:
-      logger.info("macro-F1={:.5f}\tmicro-F1={:.5f}".format(macro_f1, micro_f1))
-    else:
-      logger.info("{}\t{}".format(macro_f1, micro_f1))
+    logger.info("macro-F1={:.5f}\tmicro-F1={:.5f}".format(macro_f1, micro_f1))
+    if args.log_to_file:
       print("{}\t{}".format(macro_f1, micro_f1))
 
